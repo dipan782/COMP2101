7a4ac462e06c514acdd88a20abe4eed2734312f5
dipan782 <p.dipon35@gmail.com>
2022-09-28 12:40:25 -0400

    Bash lab 1

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-# My second script - sysinfo.sh
-cat <<EOF
-$ ./sysinfo.sh 
-Host Information:
-   Static hostname: dipan200492527
-         Icon name: computer -vm
-           Chassis: vm
-        Machine ID: f3003aed5c0947659b0a0116ed4bc8b4
-           Boot ID: bd7b1127afad42b6a29ed8f6da8e50a7
-  Operating System: Ubuntu 22.04.1 LTS 
-            Kernel: Linux 5.15.0-47-generic
-      Architecture: x86-64
-IP Addresses:
-192.168.139.152/24  fe80::b03e:206b:c904:cabb/64
-Root Filesystem Status:
-Filesystem      Size  Used Avail Use% Mounted on
-tmpfs           389M  2.0M  388M   1% /run
-/dev/sda3        20G   12G  6.8G  63% /
-tmpfs           1.9G     0  1.9G   0% /dev/shm
-tmpfs           5.0M  4.0K  5.0M   1% /run/lock
-/dev/sda2       512M  5.3M  507M   2% /boot/efi
-tmpfs           389M  2.4M  387M   1% /run/user/1000
-/dev/sr0        127M  127M     0 100% /media/dipan/CDROM
-/dev/sr1        3.6G  3.6G     0 100% /media/dipan/Ubuntu 22.04
-EOF
+
+# MY first script written
+echo "=== HOSTNAME OF MY SYSTEM ==="
+hostname
+echo " ============ "
+# HOSTNAME - Dipan200492527
+echo "=== HOSTNAMECTL ==="
+hostnamectl
+echo " ============ "
+# operating system name and the version 
+echo "=== OS NAME  ==="
+hostnamectl | grep -h "Ope"
+echo " ============ "
+# IP ADDRESS of my system
+echo "=== IP ADDRESS ==="
+hostname -I
+echo " ============ "
+# SPACE in my system
+echo "=== SPACE IN LINUX ==="
+df | grep -h "/sda"
+
